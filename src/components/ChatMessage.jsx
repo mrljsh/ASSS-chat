5e551b4a371dd3a4efabb86a58521ae63e463d8f
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-02-07 18:29:16 +0100

    style: styled the profile image and message content

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -10,29 +10,48 @@ function ChatMessage() {
         ></img>
       </UserImage>
       <MessageContent>
-        <UserName>Dragan Mrljes</UserName>
+        <UserName>Драган Мрљеш</UserName>
         <Message>First Message</Message>
       </MessageContent>
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 16px;
+`;
 
 const UserImage = styled.div`
-  max-width: 50px;
+  max-width: 42px;
+  margin-top: 8px;
 
   img {
-    max-width: 50px;
+    max-width: 42px;
     aspect-ratio: 1/1;
     object-fit: contain;
     border-radius: 50%;
   }
 `;
 
-const MessageContent = styled.div``;
+const MessageContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 8px;
+  padding: 8px 16px;
+  background-color: white;
+  border-radius: 24px;
+`;
 
-const UserName = styled.span``;
+const UserName = styled.span`
+  font-weight: 500;
+  font-size: 1.15em;
+`;
 
-const Message = styled.p``;
+const Message = styled.p`
+  font-weight: 400;
+  color: #393939;
+  margin-top: 4px;
+`;
 export default ChatMessage;
