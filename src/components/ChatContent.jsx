f54477b395beb33340c2abffbbc900ee54f8c248
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-02-07 13:59:51 +0100

    style: made sections to chatContent component

--- src/components/ChatContent.jsx
+++ src/components/ChatContent.jsx
@@ -6,7 +6,11 @@ const ChatContent = () => {
 
   return (
     <Container>
-      <p>{roomId}</p>
+      <Header>
+        <p>{roomId}</p>
+      </Header>
+      <MessagesContainer></MessagesContainer>
+      <UserInput></UserInput>
     </Container>
   );
 };
@@ -15,4 +19,15 @@ export default ChatContent;
 
 const Container = styled.div`
   background-color: #f1f1f1;
+  display: grid;
+  grid-template-rows: 64px auto min-content;
 `;
+
+const Header = styled.div`
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.075) 0px 2px 4px 0px;
+`;
+
+const MessagesContainer = styled.div``;
+
+const UserInput = styled.div``;
